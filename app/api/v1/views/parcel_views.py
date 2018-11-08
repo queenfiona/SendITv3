7f93c71e9774c779c5d332d7ebab04e01b31fb1f
queenfiona <fionaitya@gmail.com>
2018-11-08 14:09:40 +0300

    [Feature #161716337] View for post API endpoint

---
+++ app/api/v1/views/parcel_views.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, make_response, jsonify
+from flask_restful import Resource, reqparse
+from ..models import  ParcelOrder
+
+
+class ParcelOrderView(Resource, ParcelOrder):
+    """docstring for ParcelOrderView"""
+
+    def __init__(self):
+        self.parcel = ParcelOrder()
+
+    def post(self):
+        data = request.get_json(force=True)
+        user = data["user"]
+        item_shipped = data["item_shipped"]
+        origin = data["origin"]
+        destination = data["destination"]
+        weight = data["weight"]
+
+        payload = self.parcel.create_parcel_delivery_order(
+            user, item_shipped, origin, destination,weight)
+
+        return make_response(jsonify(payload), 201)
+
+    def get(self):
+        parcel_delivery_orders = self.parcel.get_all_parcel_delivery_orders()
+        payload = {
+            "message": "success",
+            "parcel orders": parcel_delivery_orders
+        }
+        return make_response(jsonify(payload), 200)
