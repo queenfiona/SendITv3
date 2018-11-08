af3dafa94fbb8f1efd504677e5d755ea51ab09ba
queenfiona <fionaitya@gmail.com>
2018-11-08 19:44:59 +0300

    [Chore #161814984] modified init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
-from flask import Api,Blueprint
+from flask import Api, Blueprint
+from .api.v1 import version_1
+
 
 def create_app():
-	app= Flask(__name__)
-	return app
+    app = Flask(__name__)
+    app.register_blueprint(version_1)
+    return app
