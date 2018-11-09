e4f73a1d479261b3da36952693f3c7171b9c187a
queenfiona <fionaitya@gmail.com>
2018-11-09 23:47:27 +0300

    [Feature #161716421] route to fetch a specific parcel delivery order]

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,9 +1,10 @@
 from flask import Blueprint
 from flask_restful import Api, Resource
-from .views.parcel_views import ParcelOrderView
+from .views.parcel_views import ParcelOrderView,SpecificParcelOrderView
 
 version_1 = Blueprint('apiv1', __name__)
 
 # Declare API and pass name of the Blueprint
 api = Api(version_1, prefix="/api/v1")
 api.add_resource(ParcelOrderView, "/parcels")
+api.add_resource(SpecificParcelOrderView,"/parcels/<int:parcel_id>")
