307ec54ef345c88690b05aaf7ee60d4f2c2bb063
queenfiona <fionaitya@gmail.com>
2018-11-10 01:39:10 +0300

    [Feature #161716368] route to fetch all orders from a specific user

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,10 +1,12 @@
 from flask import Blueprint
 from flask_restful import Api, Resource
-from .views.parcel_views import ParcelOrderView,SpecificParcelOrderView
+from .views.parcel_views import ParcelOrderView, SpecificParcelOrderView
+from .views.parcel_views import UserSpecificParcelOrderView
 
 version_1 = Blueprint('apiv1', __name__)
 
 # Declare API and pass name of the Blueprint
 api = Api(version_1, prefix="/api/v1")
 api.add_resource(ParcelOrderView, "/parcels")
-api.add_resource(SpecificParcelOrderView,"/parcels/<int:parcel_id>")
+api.add_resource(SpecificParcelOrderView, "/parcels/<int:parcel_id>")
+api.add_resource(UserSpecificParcelOrderView,"/users/<string:user_id>/parcels")
