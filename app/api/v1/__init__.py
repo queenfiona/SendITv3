66a2ef7493fb751cdf725a0c942283ffcd0bef48
queenfiona <fionaitya@gmail.com>
2018-11-11 20:27:26 +0300

    [Chore #161716449] apply pep8 format to version 1 init file

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask_restful import Api, Resource
 from .views.parcel_views import ParcelOrderView, SpecificParcelOrderView
-from .views.parcel_views import UserSpecificParcelOrderView,CancelSpecificParcelOrderView
+from .views.parcel_views import UserSpecificParcelOrderView, CancelSpecificParcelOrderView
 
 version_1 = Blueprint('apiv1', __name__)
 
@@ -9,5 +9,6 @@ version_1 = Blueprint('apiv1', __name__)
 api = Api(version_1, prefix="/api/v1")
 api.add_resource(ParcelOrderView, "/parcels")
 api.add_resource(SpecificParcelOrderView, "/parcels/<int:parcel_id>")
-api.add_resource(UserSpecificParcelOrderView,"/users/<int:user_id>/parcels")
-api.add_resource(CancelSpecificParcelOrderView,"/parcels/<int:parcel_id>/cancel")
+api.add_resource(UserSpecificParcelOrderView, "/users/<int:user_id>/parcels")
+api.add_resource(CancelSpecificParcelOrderView,
+                 "/parcels/<int:parcel_id>/cancel")
